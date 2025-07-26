aeb12fff86d24c481dbaabf6809e4e2edd5d783b
Nicole Sanchez <nicoleabsanchez@gmail.com>
2025-07-25 21:08:35 -0500

    Step one added

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print('hello world')
+import cv2
+import numpy as np
+import face_recognition
+
+imgElon = face_recognition.load_image_file('Images/musk1.jpg')
+imgElon =cv2.cvtColor(imgElon, cv2.COLOR_BGR2RGB)
+
+imgTest = face_recognition.load_image_file('Images/musktest.jpg')
+imgTest = cv2.cvtColor(imgTest, cv2.COLOR_BGR2RGB)
+
+cv2.imshow('Elon Musk', imgElon)
+cv2.imshow('Elon Test', imgTest)
+cv2.waitKey(0)
