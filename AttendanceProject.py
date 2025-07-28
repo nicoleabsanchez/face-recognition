66d70b4b93e73c835415ef28b26b6a32c67e5c9c
Nicole Sanchez <nicoleabsanchez@gmail.com>
2025-07-28 12:30:34 -0500

    new folder

---
+++ AttendanceProject.py
@@ -0,0 +1,50 @@
+import cv2
+import numpy as np
+import face_recognition
+import os
+
+path = 'imgAtt'
+images = []
+classNames = []
+myList = os.listdir(path)
+print(myList)
+for cl in myList:
+    currImg = cv2.imread(f'{path}/{cl}')
+    images.append(currImg)
+    classNames.append(os.path.splitext(cl)[0])
+print(classNames)
+
+def findEncodings(images):
+    encodeList= []
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+
+encodeListKnown = findEncodings(images)
+print(len(encodeListKnown))
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    success, img = cap.read()
+    imgS = cv2.resize(img, (0,0), None, 0.25, 0.25)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+
+    faceCurrFrame = face_recognition.face_locations(imgS)
+    encodeCurrFram = face_recognition.face_encodings(imgS, faceCurrFrame)[0]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
