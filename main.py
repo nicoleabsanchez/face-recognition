c7b40772292af618c588c2a39c494cc9ee7b9902
Nicole Sanchez <nicoleabsanchez@gmail.com>
2025-07-28 11:48:15 -0500

    hi

--- main.py
+++ main.py
@@ -8,6 +8,17 @@ imgElon =cv2.cvtColor(imgElon, cv2.COLOR_BGR2RGB)
 imgTest = face_recognition.load_image_file('Images/musktest.jpg')
 imgTest = cv2.cvtColor(imgTest, cv2.COLOR_BGR2RGB)
 
+faceLoc =face_recognition.face_locations(imgElon)[0]
+encodeElon = face_recognition.face_encodings(imgElon)[0]
+cv2.rectangle(imgElon, (faceLoc[3], faceLoc[0]), (faceLoc[1], faceLoc[2]), (255,0,255))
+print(faceLoc)
+
+faceLocTest = face_recognition.face_locations(imgTest)[0]
+encodeTest = face_recognition.face_encodings(imgTest)[0]
+cv2.rectangle(imgTest, (faceLoc[3], faceLoc[0]), (faceLoc[1], faceLoc[2]), (255,0,255))
+print(faceLocTest)
+
+
 cv2.imshow('Elon Musk', imgElon)
 cv2.imshow('Elon Test', imgTest)
 cv2.waitKey(0)
